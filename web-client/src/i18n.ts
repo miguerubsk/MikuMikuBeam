519aab6f5e79dbbdbee3d5145074126d8d5c884e
miguerubsk <miguerubsk@gmail.com>
2026-02-17 15:07:23 +0000

    merge from test

---
+++ web-client/src/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
+    },
+    detection: {
+      order: ["querystring", "navigator", "htmlTag"],
+      caches: [], // Disable caching to always detect from browser/url
+    },
+  });
+
+export default i18n;
